1855154be37e39e0f961ddc8d29fc040c0153ce4
MGainullin <ya.azamark@yandex.ru>
2022-06-20 10:20:25 +0300

    Компонент Levels
    App слушает событие selected, компонента Levels, по которому пушит новый вызов в стэк

--- src/App.vue
+++ src/App.vue
@@ -1,72 +1,44 @@
 <template>
-    <p> Вызов {{picked}}</p>
-    <div class="levels"
-        v-for="level in levels"
-        :key="level.name"
-    >
-        <input type="radio"
-            v-model="elevator.currentLvl" 
-            :value="level.value"
-        >
-        <input type="radio"
-            v-model="picked" 
-            :id="level.name"
-            :value="level.value"
-            @change="select"
-        >
-        <label :for="level.name">{{level.name}}</label>
-    </div>
-    <p> Текущий {{elevator.currentLvl}}</p>
-
+    <levels
+       :model-value="picked"
+       :levels="levels"
+       @selected="pushStack"
+    />
 </template>
 
 <script>
+import Levels from "./components/Levels.vue"
+import Elevator from "./components/Elevator.vue"
 export default {
+    components: {Levels, Elevator},
     data() {
         return { 
-            levels: [
-                {name: 'One',value: 1},
-                {name: 'Two',value: 2},
-                {name: 'Three',value: 3},
-                {name: 'Four',value: 4},
-                {name: 'Five',value: 5}
-            ],
             picked: 1,
             callStack: [],
-            elevator: {
-                action: 'ready',
-                currentLvl: 1,
-                targetLvl: 1
-            }
+            levels: [
+                {name: 'One', value: 1},
+                {name: 'Two', value: 2},
+                {name: 'Three', value: 3},
+                {name: 'Four', value: 4},
+                {name: 'Five', value: 5}
+            ],
         }
     },
     methods: {
-        select() {
-            console.log(this.picked);
-            let find = this.callStack.find( el => {
-                return el === this.picked
+        pushStack($data) {
+            let find =  this.callStack.find(el => {
+                return el === $data
             });
             if(find) {
                 console.log('Вызов уже в очереди');
             } else {
-                this.callStack.push(this.picked);
-                console.log(`Вызов на ${this.picked} этаж`);
-                console.log(`Очередь вызовов ${this.callStack}`);
-                if(this.elevator.action === 'ready') {
-                    this.elevator.targetLvl = this.callStack[0];
-                }
+                this.callStack.push($data);
+                console.log(this.callStack);
             }
-        }
-    },
-    watch: {
-        elevator: {
-            handler(val, oldVal) {
-                if(this.elevator.targetLvl !== this.elevator.currentLvl) {
-                    this.elevator.currentLvl = this.elevator.targetLvl
-                    this.callStack.shift();
-                }
-            },
-            deep: true
+        },
+        shiftStack() {
+            console.log('Удаление первого элемента массива')
+            this.callStack.shift();
         }
     }
 }
